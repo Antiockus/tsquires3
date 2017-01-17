b2b641208dfd577a5653e32642d2d4a051b1adcd
Tim Squires <antiockus@gmail.com>
2017-01-17 14:07:09 -0500

    final commit

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -7,13 +7,21 @@
 @section('content')
     <div class="container">
         <div class="row">
+            <div class="col-md-12 text-center panel panel-default">
+                <h2>Welcome to the Site!</h2>
+                <p>This site was created for my portfolio. It is meant to showcase different skills and abilities
+                    that I have learned over a period of study.</p>
+
+            </div>
+        </div>
+        <div class="row">
             @if( count($posts) >= 1)
                 @foreach($posts as $post)
                     <div class="card panel panel-default">
-                        <div class="panel-header">
-                            <h1><a href="{{url("/posts/$post->id")}}">{{ $post->title }}</a></h1>
-                        </div>
                         <div class="panel-body">
+
+                            <h1><a href="{{url("/posts/$post->id")}}">{{ $post->title }}</a></h1>
+                            <h4>{{ $post->created_by }}</h4>
                             <sub>{{ $post->published_at }}</sub>
                             <p>{!!  str_limit($post->body, 100, '...') !!}</p>
                         </div>
